5a29d60451495a38f964c563204e4e55f74a317a
ruby1610 <marieke.raasveldt@gmail.com>
2025-04-14 13:01:19 +0200

    removed obj folder, .vscode folder and executable and added gitignore file

--- obj/Server/Client.d
+++
@@ -1,7 +0,0 @@
-obj/Server/Client.o: src/Server/Client.cpp src/Server/./Client.hpp \
- src/Server/././Request.hpp src/Server/././../Parsing/RequestParser.hpp \
- src/Server/././Server.hpp src/Server/./././Request.hpp \
- src/Server/././../Networks/Socket.hpp \
- src/Server/././../Networks/Epoll.hpp \
- src/Server/././../Networks/Socket.hpp src/Server/././Response.hpp \
- src/Server/./././Server.hpp src/Server/./../Networks/Epoll.hpp
