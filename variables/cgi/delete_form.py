4a66c04a88f7ee4e2cf0ba47df4709b9f3a42030
andmadri <andresmadridu@outlook.com>
2025-05-04 11:10:12 +0200

    POST works need to work on DELETE

---
+++ variables/cgi/delete_form.py
@@ -0,0 +1,8 @@
+import os
+import urllib.parse
+
+UPLOAD_DIR = "variables/uploads"
+
+query_string = os.environ.get("QUERY_STRING", "")
+params = urllib.parse.parse_qs(query_string)
+filename_list = params.get("file")
