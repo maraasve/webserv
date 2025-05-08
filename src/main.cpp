52b701a536a72d973a9e31fccecadd865c276f08
marieke <marieke.raasveldt@gmail.com>
2025-05-08 16:14:35 +0000

    added signalHandler, seems to work

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include "./Networks/Epoll.hpp"
 #include "./Networks/Socket.hpp"
-#include <iostream>
 #include "./Parsing/ConfigParser.hpp"
-#include "./Server/WebServer.hpp"	
+#include "./Server/WebServer.hpp"
+#include <iostream>
+#include <csignal>
 
 #define PORT 8080
 
+volatile sig_atomic_t shutdownRequested = 0;
+
+void	sigHandler(int signal) {
+	(void)signal;
+	shutdownRequested = 1;
+}
+
 int main(int argc, char *argv[]) {
 	std::string configuration_file;
-	if (argc != 2) {
+	if (argc != 2) { //what if argc > 2?
 		configuration_file = "./configuration_files/default.conf";
 	} else {
 		configuration_file = argv[1];
 	}
+	signal(SIGINT, sigHandler);
+	signal(SIGPIPE, SIG_IGN);
 	try {
 		WebServer webserver(configuration_file);
 		webserver.run();
